7e05dafbe6be9d042a3584912a2b7d3c51747b92
Michele <ercolinimichele@outlook.it>
2023-09-25 12:07:25 +0200

    swiper funzionante

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,25 +1,17 @@
 /* Swiper Header*/
 
 let swiper = new Swiper(".mySwiper", {
-    grabCursor: true,
     effect: "creative",
     loop: true,
-    speed: 300,
-    mousewheel:{
-        invert: false,
-    },
+    speed: 500,
+    simulateTouch:false,
     /* autoplay: {
-        delay: 5000,
+        delay: 2500,
         disableOnInteraction: false,
     }, */
-    pagination:{
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-    },
     creativeEffect: {
         prev: {
-            shadow: true,
+            shadow: false,
             origin: "left center",
             translate: ["-5%", 0, -200],
             rotate: [0, 100, 0],
@@ -30,4 +22,36 @@ let swiper = new Swiper(".mySwiper", {
             rotate: [0, -100, 0],
         },
     },
+    pagination:{
+        el: '.swiper-pagination',
+        clickable: false,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
+
+let imgHeader = document.querySelector('.imgHeader');
+let swiperButtonPrev = document.querySelector('.swiper-button-prev');
+let swiperButtonNext = document.querySelector('.swiper-button-next');
+
+let css_of_before = window.getComputedStyle(imgHeader, '::before');
+let count = 3;
+
+swiperButtonPrev.addEventListener("click", changeImg);
+swiperButtonNext.addEventListener("click", changeImg);
+
+function changeImg(){
+    count++;
+    if(count%3==1){
+        imgHeader.style.setProperty('--beforeImg', 'url(/public/media/liberta.png)');
+    }else if(count%3==2){
+        imgHeader.style.setProperty('--beforeImg', 'url(/public/media/bigben2.png)');
+    }else{
+        imgHeader.style.setProperty('--beforeImg', 'url(/public/media/eiffel.png)');
+    }
+    console.log(count);
+}
+
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <header class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="col-12 col-md-8">
+    <div class="row justify-content-center imgHeader">
+        <div class="col-12 col-lg-10">
             <div class="swiper mySwiper">
                 <div class="swiper-wrapper">
                     <div class="swiper-slide">
@@ -39,34 +39,12 @@
                             </div>
                         </div>
                     </div>
-                    <div class="swiper-slide">
-                        <div class="card card4">
-                            <div class="card-content">
-                                <h3 class="card-title">Title</h3>
-                                <h5>Category</h5>
-                                <p class="card-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum vero vitae veniam placeat nam, voluptas, accusamus ab aliquam libero reiciendis quod enim facilis ea, perspiciatis sapiente deleniti eum dolor quam.</p>
-                                <div class="d-flex justify-content-end">
-                                    <a class="button" href="">Learn more</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {{-- <div class="swiper-slide">
-                        
-                    </div>
-                    <div class="swiper-slide">
-                        
-                    </div>
-                    <div class="swiper-slide">
-                        
-                    </div>
-                    <div class="swiper-slide">
-                        
-                    </div>
-                    <div class="swiper-slide">
-                        
-                    </div> --}}
                 </div>
+                <!-- Add Pagination -->
+                <div class="swiper-pagination"></div>
+                <!-- Add Navigation -->
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
             </div>
         </div>
     </div>
@@ -75,4 +53,5 @@
             <h1 class="text-center">CapeTrip</h1>
         </div>
     </div>
+    <div class="row vh-100"></div>
 </header>
